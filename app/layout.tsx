b2c7a48be5373c5c8423ce0be77795eac3c6178b
guilhermezils <guilhermezils@icloud.com>
2024-07-22 21:43:40 -0400

    css+fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,6 @@
+import '@/app/ui/global.css';
+import { inter } from '@/app/ui/fonts';
+ 
 export default function RootLayout({
   children,
 }: {
@@ -5,7 +8,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${inter.className} antialiased`}>{children}</body> {/* Here, you're also adding the Tailwind antialiased class which smooths out the font. This is a good practice to follow when using Tailwind CSS. */}
     </html>
   );
 }
